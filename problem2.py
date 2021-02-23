173ff21111c13eb173462a5be09b3f3d03d03a73
Tyler Ku <tyler.ku23@ycdsbk12.ca>
2021-02-23 15:20:38 +0000

    Finished both programs

--- problem2.py
+++ problem2.py
@@ -1,1 +1,24 @@
+"""
+-------------------------------------------------------------------------------
+Name:   problem2.py
+Purpose:  a description of your program
+ 
+Author: Ku.T
+ 
+Created:  03, 02, 2021
+------------------------------------------------------------------------------
+"""
+#Greetings
+print("Welcome to the Triangle Checker")
 
+#User inputs
+first_side = int(input("Enter the length of the first side: "))
+second_side = int(input("Enter the length of the second side: "))
+third_side = int(input("Enter the length of the third side: "))
+
+#If statements bunched together
+if first_side + second_side > third_side or second_side + third_side > first_side or third_side + first_side > second_side:
+  print("The figure is a triangle.")
+
+else:
+  print("The figure is not a triangle.")
